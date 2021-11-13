2263bfbb8c2c1bd14cca48d31f83bfc5f7dd4b66
root <root@dell.localdomain>
2021-11-13 10:47:59 +0300

    final

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7#pyi-!p*!@j=_@6_!_hao^jmyl129xa*%a=@@4fr2rlc#9ln7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -122,4 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
